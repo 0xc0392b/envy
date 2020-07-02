959aa58c6e35bb46c6073ce953f6050462601587
William <william.santos@hotmail.co.uk>
2020-07-02 12:55:51 +0100

    add GetEnvAsInt

--- int.go
+++ int.go
@@ -5,6 +5,12 @@ import (
   "strconv"
 )
 
+// GetEnvAsInt simply wraps GetEnvAsInt32.
+func GetEnvAsInt(k string, d int) int {
+  n := GetEnvAsInt32(k, int32(d))
+  return int(n)
+}
+
 // GetEnvAsFloat32 takes a string key k and a int32 default value d and returns
 // the environment variable with the key k if it is not the empty string
 // else returns the default value. Returns -1 if cannot parse value as int.
